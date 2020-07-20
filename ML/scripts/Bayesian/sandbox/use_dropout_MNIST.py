41cdefc044b37365ced9bd75950e2628e7badf80
Tashalee <tashalee@sas.upenn.edu>
2020-07-20 15:29:13 -0400

    stuff I should have done a long time ago

--- ML/scripts/Bayesian/sandbox/use_dropout_MNIST.py
+++ ML/scripts/Bayesian/sandbox/use_dropout_MNIST.py
@@ -12,11 +12,12 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Activation, Dropout
+from tensorflow.keras import Model
+#from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout, Softmax, ReLU, Flatten #Flatten, Activation
 from tensorflow.keras.utils import normalize, to_categorical
 
-import gc
+import gc, time
 gc.enable()
 
 tfk = tf.keras
@@ -36,27 +37,45 @@ X_test = normalize(X_test, axis=1)
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
+"""
 # Build Sequential Model with Dropout
 model_dropout = Sequential()
 
 model_dropout.add(Flatten(input_shape=(28, 28)))
 model_dropout.add(Dense(N_HIDDEN))
+
 model_dropout.add(Dropout(DROPOUT))
 model_dropout.add(Activation('relu'))
 model_dropout.add(Dense(N_HIDDEN))
 model_dropout.add(Dropout(DROPOUT))
+
 model_dropout.add(Activation('relu'))
 model_dropout.add(Dense(N_CLASSES))
 model_dropout.add(Activation('softmax'))
 
 model_dropout.summary()
+"""
+inputs = Flatten(input_shape=(28, 28))
+#inputs = Input(shape=(X_train.shape[1]),)
+#inter = Dense(N_CLASSES)(inputs)
+inter = Dropout(DROPOUT)(inputs, training=True)
+#inter = ReLU()
+inter = Dense(N_CLASSES, activation='relu')(inter)
+inter = Dropout(DROPOUT)(inter, training=True)
+inter = Dense(128, activation='relu')(inter)
+output = Dense(N_CLASSES, activation='softmax')(inter)
 
+model_dropout = Model(inputs=inputs, outputs=output)
+
+# We’re trying to predict classes, we use categorical crossentropy as our loss function
 # Compile model
 model_dropout.compile(
     loss='categorical_crossentropy',
     optimizer=OPTIMIZER,
     metrics=['accuracy'])
-    
+
+start_time = time.time()
+# Start Training
 history_dropout = model_dropout.fit(
     X_train,
     y_train,
@@ -65,6 +84,8 @@ history_dropout = model_dropout.fit(
     validation_split=VALIDATION_SPLIT,
     verbose = 1,
     shuffle=True)
+
+running_time = time.time() - start_time
 
 test_loss, test_acc = model_dropout.evaluate(X_test, y_test)
 
